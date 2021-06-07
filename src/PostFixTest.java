e68d6e447c8737be11fdd0c9ecb6a7c61b8d3746
Anh_Nguyen <Anh.NguyenHuong@Student.HTW-Berlin.de>
2021-06-07 17:05:04 +0200

    adding test cases

--- src/PostFixTest.java
+++ src/PostFixTest.java
@@ -3,11 +3,68 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PostFixTest {
+	
+	// Testing finger-exercises 
 
-//	@Test
-//	void test() {
-//		PostFix a = new PostFix();
-//		assertEquals("Not yet implemented");
-//	}
+	@Test
+	void test_a() throws StackUnderflow, StackOverflow {
+		PostFix a = new PostFix();	
+		assertEquals(5, a.evaluate("1 2 * 3 +"));
+		}
+	
+	
+	@Test
+	void test_b() throws StackUnderflow, StackOverflow {
+		PostFix a = new PostFix();
+		assertEquals(7, a.evaluate("1 2 3 * +"));
+
+	}
+	
+	@Test
+	void test_c() throws StackUnderflow, StackOverflow {
+		PostFix a = new PostFix();
+			assertEquals(-78, a.evaluate("1 2 + 3 4 ^ -"));
+	}
+	
+	@Test
+	void test_d() throws StackUnderflow, StackOverflow {
+		PostFix a = new PostFix();
+        assertEquals(-11, a.evaluate("1 2 ^ 3 4 * -"));
+
+	}
+	
+	@Test
+	void test_e() throws StackUnderflow, StackOverflow {
+		PostFix a = new PostFix();
+		assertEquals(-1011, a.evaluate("1 2 3 * + 4 5 ^ - 6 +"));
+
+	}
+	
+	@Test
+	void test_f() throws StackUnderflow, StackOverflow {
+		PostFix a = new PostFix();
+		assertEquals(9.25, a.evaluate("1 2 + 3 * 4 5 6 - ^ +"));
+
+	}
+	
+	@Test
+	void test_g() throws StackUnderflow, StackOverflow {
+		PostFix a = new PostFix();
+		assertEquals(98.75, a.evaluate("1 2 + 3 4 / + 5 + 6 7 8 + * +"));
+
+	}
+	
+	@Test
+	void test_h() throws StackUnderflow, StackOverflow {
+		PostFix a = new PostFix();
+		assertEquals(-1, a.evaluate("9 1 - 2 - 3 2 * - 1 -"));
+
+	}
+	
+	
+	
+	
+	
+	
 
 }
