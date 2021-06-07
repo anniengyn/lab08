e463a41a33e0231d4c11bf42970c57680ec76c45
Ysfz00 <82992768+Ysfz00@users.noreply.github.com>
2021-06-07 19:17:51 +0200

    Update StackOverflow.java

--- src/StackOverflow.java
+++ src/StackOverflow.java
@@ -5,6 +5,8 @@ public class StackOverflow extends Exception{
 	 */
 	private static final long serialVersionUID = -530906547635136353L;
 
-	public StackOverflow () {}
-
+	public StackOverflow( String message )
+    {
+ 	 super( message );  // Calls Exception's constructor    
+    }
 }
