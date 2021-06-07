801c936585b79a74b78896501f0815f181d2fc9f
Ysfz00 <82992768+Ysfz00@users.noreply.github.com>
2021-06-07 19:18:08 +0200

    Update StackUnderflow.java

--- src/StackUnderflow.java
+++ src/StackUnderflow.java
@@ -5,7 +5,9 @@ public class StackUnderflow extends Exception{
 	 */
 	private static final long serialVersionUID = 7129268257805783311L;
 
-	public StackUnderflow() {
-	}
+	public StackUnderflow( String message )
+    {
+ 	 super(message);  // Calls Exception's constructor    
+    }
 
 }
