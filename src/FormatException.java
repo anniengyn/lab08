aa4c04ebb856d89070f5aea0a9c407b072e2050d
Anni <82827750+anniengyn@users.noreply.github.com>
2021-06-09 21:44:29 +0200

    Add files via upload

---
+++ src/FormatException.java
@@ -0,0 +1,14 @@
+
+public class FormatException extends Exception{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1697121205572195775L;
+
+	public FormatException( String message )
+    {
+ 	 super( message );  // Calls Exception's constructor    
+    }
+}
